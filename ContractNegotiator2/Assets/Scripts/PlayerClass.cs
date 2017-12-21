9f01ea209b40e2ce9f66e2856077624ee0c5d413
Dylan <dylan@PIZZAGALORE.localdomain>
2017-12-21 14:34:13 -0500

    GameController and Player Singleton added

---
+++ ContractNegotiator2/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass {
+
+	static private PlayerClass _instance;
+	static public PlayerClass Instance {
+		get 
+		{
+			if (_instance == null) {
+				_instance = new PlayerClass ();
+			}
+			return _instance;
+		}
+	} 
+
+	private PlayerClass() {}
+
+	public GameController gCtrl;
+
+	private int _score = 0;
+	private int _life = 3;
+
+	public int Score {
+		get { return _score; }
+		set
+		{ 
+			_score = value;
+			gCtrl.updateUI ();
+		}
+	}
+
+	public int Life{
+		get { return _life; }
+		set { 
+			_life = value;
+
+			if (_life <= 0) {
+				gCtrl.gameOver ();
+			} else
+			{
+				gCtrl.updateUI ();
+			}
+		}
+	}
+}
