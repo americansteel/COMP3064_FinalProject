05cabdb2317b1803c89651ea08cd909fdf3c1c56
seanparade <seanparade@gmail.com>
2017-12-21 14:55:28 -0500

    coroutines and bus honk

---
+++ ContractNegotiator2/Assets/Scripts/Controllers/SpawnRoutines.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoutines : MonoBehaviour {
+
+	public IEnumerator AddCar(GameObject car, GameObject bus)
+	{
+		int time = Random.Range (1, 5);
+		yield return new WaitForSeconds ((float)time);
+		Instantiate (car);
+		StartCoroutine ("AddCar");
+		if(time > 3)
+		{
+			AddBus(bus);
+		}
+	}
+
+	public IEnumerator AddBus(GameObject bus)
+	{
+		int time = Random.Range (3, 10);
+		yield return new WaitForSeconds ((float)time);
+		Instantiate (bus);
+	}
+}
